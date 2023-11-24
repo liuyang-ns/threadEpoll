3437cfd35f0a68bab99c8227a5a94fe0112f04b8
liuyang-ns <1272573593@qq.com>
2023-11-25 02:29:35 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "Server.h"
+
+int main(int argc, char* argv[])
+{
+	if (argc < 3)
+	{
+		printf("./a.out port path\n");
+		return -1;
+	}
+	unsigned short port = atoi(argv[1]);
+	//切换服务器的工作路径
+	chdir(argv[2]);
+
+	//初始化监听的套接字
+	int lfd = initListenFd(port);
+
+	//启动服务器程序
+	epollRun(lfd);
+
+	return 0;
+}
